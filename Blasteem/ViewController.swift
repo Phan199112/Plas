685be5f85713413fff30bcd2fc3e47fd2deeb07a
Thai <kic4329@gmail.com>
2016-08-18 02:23:55 +0700

    Test Commit from Xcode

--- Blasteem/ViewController.swift
+++ Blasteem/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
-
+import Segmentio
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segmentioView: Segmentio!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,71 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    //Segment Setting Related
+    private func segmentioOptions() -> SegmentioOptions {
+        let imageContentMode = UIViewContentMode.ScaleAspectFit
+        return SegmentioOptions(
+            backgroundColor: UIColor.clearColor(),
+            maxVisibleItems: 5,
+            scrollEnabled: false,
+            indicatorOptions: segmentioIndicatorOptions(),
+            horizontalSeparatorOptions: segmentioHorizontalSeparatorOptions(),
+            verticalSeparatorOptions: segmentioVerticalSeparatorOptions(),
+            imageContentMode: imageContentMode,
+            labelTextAlignment: .Center,
+            segmentStates: segmentioStates()
+        )
+    }
+    
+    private func segmentioStates() -> SegmentioStates {
+        let font = UIFont.systemFontOfSize(13);
+        return SegmentioStates(
+            defaultState: segmentioState(
+                backgroundColor: UIColor.clearColor(),
+                titleFont: font,
+                titleTextColor: UIColor.whiteColor()
+            ),
+            selectedState: segmentioState(
+                backgroundColor: UIColor.clearColor(),
+                titleFont: font,
+                titleTextColor: UIColor.whiteColor()
+            ),
+            highlightedState: segmentioState(
+                backgroundColor: UIColor.clearColor(),
+                titleFont: font,
+                titleTextColor: UIColor.whiteColor()
+            )
+        )
+    }
+    
+    private func segmentioState(backgroundColor backgroundColor: UIColor, titleFont: UIFont, titleTextColor: UIColor) -> SegmentioState {
+        return SegmentioState(backgroundColor: backgroundColor, titleFont: titleFont, titleTextColor: titleTextColor)
+    }
+    
+    private func segmentioIndicatorOptions() -> SegmentioIndicatorOptions {
+        return SegmentioIndicatorOptions(
+            type: .Bottom,
+            ratio: 1,
+            height: 5,
+            color: UIColor.whiteColor()
+        )
+    }
+    
+    private func segmentioHorizontalSeparatorOptions() -> SegmentioHorizontalSeparatorOptions {
+        return SegmentioHorizontalSeparatorOptions(
+            type: .TopAndBottom,
+            height: 0,
+            color: UIColor.whiteColor()
+        )
+    }
+    
+    private func segmentioVerticalSeparatorOptions() -> SegmentioVerticalSeparatorOptions {
+        return SegmentioVerticalSeparatorOptions(
+            ratio: 0,
+            color: UIColor.whiteColor()
+        )
+    }
 
 }
 
