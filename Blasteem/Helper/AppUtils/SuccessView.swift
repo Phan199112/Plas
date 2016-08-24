23d76d06340fc6b3c8a9c3b141b4410677e6f300
Thai <kic4329@gmail.com>
2016-08-25 06:23:50 +0700

    VideoPage Working

---
+++ Blasteem/Helper/AppUtils/SuccessView.swift
@@ -0,0 +1,21 @@
+//
+//  SuccessView.swift
+//  Blasteem
+//
+//  Created by k on 8/25/16.
+//  Copyright © 2016 beneta. All rights reserved.
+//
+
+import UIKit
+
+class SuccessView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
